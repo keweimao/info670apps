aac00397ecc42b3c445603b83f2573fbf6d8b3bd
Weimao Ke <weimao.ke@gmail.com>
2024-04-16 21:08:55 -0400

    week 2 exercise

---
+++ MyFirstReactNative/App copy.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+        <ScrollView>
+          <Text>INFO 300</Text>
+        </ScrollView>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ week2apps/App.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'; 
+import {Text, View, Image, Button, FlatList} from 'react-native';
+
+const Dog = props => {
+  const [isHungry, setIsHungry] = useState(true); 
+  return (
+    <View>
+      <Image source={require("./img/icon.png")} style={{width:50,height:50}}/>
+      <Text>Hello, I am {props.name}, and I am {isHungry? "hungry" : "full"}!</Text>
+      <Button 
+        onPress={() => {
+          setIsHungry(false); 
+        }}
+        title={isHungry? "Feed me!" : "Thank you!"}
+      />
+    </View>);
+}
+
+const Dogs = () => {
+  return (
+    <View>
+      <Text>Welcome All!</Text>
+      <Dog name="John" />
+      <Dog name="Cocoa" />
+      <Dog name="Joseph" />
+    </View>
+  );
+}
+
+
+const DogList = () => {
+  return (
+    <View>
+      <FlatList 
+        data={[
+          {name: "John"}, 
+          {name: "Cocoa"}, 
+          {name: "Joe"},
+        ]}
+        renderItem={({item}) => <Dog name={item.name} />}
+      />
+    </View>
+  ); 
+}
+
+export default Dogs; 
